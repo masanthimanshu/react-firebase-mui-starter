5e68d404ba13bd1c1e59319f7801d72b8f09f8e3
Himanshu Masant <himanshumasant@uxdlabtech.com>
2022-09-24 20:45:24 +0530

    update

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import { Router } from "./Router";
-import { Theme } from "./Theme-file";
+import { Theme } from "./Theme";
 
 const root = createRoot(document.getElementById("root"));
 
